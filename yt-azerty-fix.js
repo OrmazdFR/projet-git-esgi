032728c85989be321b1d58e651ee3ea895084dc4
Ormazd <pbisset@protonmail.com>
2022-01-04 16:58:50 +0100

    Adding decreasingCaption support

--- yt-azerty-fix.js
+++ yt-azerty-fix.js
@@ -2,13 +2,16 @@ document.addEventListener('keydown', (e) => {
 	e = e || window.event;
 	const el = document.activeElement.id
 	if (el != "search" && el != "contenteditable-root") {
-		if (e.keyCode == 59) {
-			// Next Frame (while paused) + Increase playback rate 59
-			var kcode = 190;
-			document.dispatchEvent(new KeyboardEvent("keydown", {
+		switch (e.keyCode) {
+			case 59:
+			case 169:
+				// Next Frame (while paused) + Increase playback rate 59
+				var kcode = e.keyCode == 59 ? 190 : 173;
+				document.dispatchEvent(new KeyboardEvent("keydown", {
 				'keyCode': kcode,
 				'shiftKey': e.shiftKey
-			}))
+				}))
+				break;
 		}
 	}
 });
