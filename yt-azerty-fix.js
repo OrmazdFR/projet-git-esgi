ae5e5669a947813bc178f75706d1e04ef6d187bd
Ormazd <pbisset@protonmail.com>
2022-01-04 16:40:30 +0100

    Active element fix
    Will not trigger next frame if pressing ";" if searching for a video or writing a comment

--- yt-azerty-fix.js
+++ yt-azerty-fix.js
@@ -1,10 +1,13 @@
 document.addEventListener('keydown', (e) => {
 	e = e || window.event;
-	if (e.keyCode == 59) {
-		// Next Frame (while paused) + Increase playback rate 59
-		var kcode = 190;
-		document.dispatchEvent(new KeyboardEvent("keydown", {
-			'keyCode': kcode
-		}))
+	const el = document.activeElement.id
+	if (el != "search" && el != "contenteditable-root") {
+		if (e.keyCode == 59) {
+			// Next Frame (while paused) + Increase playback rate 59
+			var kcode = 190;
+			document.dispatchEvent(new KeyboardEvent("keydown", {
+				'keyCode': kcode
+			}))
+		}
 	}
 });
